0dd8bf390f85fd209b73064dded9a1672b820dc4
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-12-02 12:36:23 +0100

    hwFeddback

--- src/components/FeedbackOption/FeedbackOptions.jsx
+++ src/components/FeedbackOption/FeedbackOptions.jsx
@@ -1,6 +1,7 @@
+import css from './FeedbackOption.module.css';
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     
-    return options.map((elem, idx) => (<button key={idx} className="option" type="button" name={elem} onClick={onLeaveFeedback}>{elem}</button>));
+    return options.map((elem, idx) => (<button key={idx} className={css.option} type="button" name={elem} onClick={onLeaveFeedback}>{elem}</button>));
         
 };
 
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,7 @@
-const Statistics = ({ good, neutral, bad, total=0, positiveFeedback=0 }) => {
-    return (
+import Notification from "components/Notification/Notification";
+
+const Statistics = ({ good, neutral, bad, total, positiveFeedback }) => {
+    return total===0 ? <Notification message="There is no feedback"/> : (
         <div>
             <p>Good: {good}</p>
             <p>Neutral: {neutral}</p>
